05c9a409c3a6d3817ef2afaad0afaadc33cb3343
ak1103dev <ak.apichan1103@gmail.com>
2018-12-29 16:39:46 +0700

    :construction: init project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ngen/pages/home/home.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  final appTitle = 'NGEN';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: appTitle),
+    );
+  }
+}
---
+++ lib/pages/daily/daily.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DailyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:  Center(
+        child: Text(
+          'Hello, world!',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+}
